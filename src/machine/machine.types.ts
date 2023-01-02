37f3ea1a477d1e0c77841d0dc8e4a50e1ef2200a
chlbri <bri_lvi@icloud.com>
2023-01-02 10:26:42 +0000

    ok

--- src/machine/machine.types.ts
+++ src/machine/machine.types.ts
@@ -1,3 +1,11 @@
-export type Context = {};
+export type Context = {
+  navigation?: {
+    history: string[];
+    current: string;
+  };
+};
 
-export type Events = {};
+export type Events =
+  | { type: 'NAVIGATE.POP' }
+  | { type: 'NAVIGATE.PUSH' }
+  | { type: 'NAVIGATE.POP' };
